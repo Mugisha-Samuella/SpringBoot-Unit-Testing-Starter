dbe0aa93b42ba7ed1c2cb24a00530316b407bfd8
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-10-27 08:27:45 +0200

    feat[init] : application initialisation

---
+++ src/test/java/com/app/School_MIS/SchoolMisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.School_MIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolMisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
