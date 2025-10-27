a2eb1037fe3227e52db692f11a00e361918ae404
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2025-10-27 11:56:49 +0200

    feat : student controller setup

---
+++ src/main/java/com/app/School_MIS/controllers/StudentController.java
@@ -0,0 +1,38 @@
+package com.app.School_MIS.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.School_MIS.models.Student;
+import com.app.School_MIS.services.StudentService;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+    
+    @PostMapping
+    public Student saveStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{email}")
+    public Student getStudentByEmail(@PathVariable String email) {
+        return studentService.getStudentByEmail(email);
+    }
+}
